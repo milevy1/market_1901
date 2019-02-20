de6b6ca402e064109f8dc7777b1e573770c8fe35
Matt Levy <milevy1@gmail.com>
2019-02-20 09:16:14 -0700

    Initial commit

--- test/market_test.rb
+++ test/market_test.rb
@@ -5,4 +5,12 @@ require './lib/vendor'
 
 class MarketTest < Minitest::Test
 
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
 end
