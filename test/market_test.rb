e76cad0ee4e8d882465da36bbad8bebc7cdb08db
Matt Levy <milevy1@gmail.com>
2019-02-20 09:29:30 -0700

    Added test_sorted_item_list

--- test/market_test.rb
+++ test/market_test.rb
@@ -61,4 +61,14 @@ class MarketTest < Minitest::Test
     assert_equal expected_2, @market.vendors_that_sell("Banana Nice Cream")
   end
 
+  def test_sorted_item_list
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+
+    expected = ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"]
+
+    assert_equal expected, @market.sorted_item_list
+  end
+
 end
